58841dd9875ed7d9d2735d38ec72554340abddac
David <davidramirez9210@gmail.com>
2021-05-08 11:56:23 -0300

    barra,listadoProyecto,nuevoProyecto,FormTarea

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NuevoProyecto from '../proyectos/NuevoProyecto';
+import ListadoProyectos from '../proyectos/ListadoProyectos';
 
 const Sidebar = () => {
     return ( 
@@ -7,9 +8,10 @@ const Sidebar = () => {
             <h1>MERN<span>Tasks</span></h1>
 
             <NuevoProyecto/>
-            
+
             <div className="proyectos">
                 <h2>Tus Proyectos</h2>
+                <ListadoProyectos/>
             </div>
         </aside>
      );
--- src/components/proyectos/NuevoProyecto.jsx
+++ src/components/proyectos/NuevoProyecto.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const NuevoProyecto = () => {
+
+    //State para proyecto
+    const [proyecto,guardarProyecto] = useState({
+        nombre:''
+    });
+
+    //Extraer nombre de proyecto
+    const {nombre} = proyecto;
+
+    //Lee el input 
+    const onChangeProyecto = e => {
+        guardarProyecto({
+            ...proyecto,
+            [e.target.name] :e.target.value
+        })
+    }
+
+    //Cuando el usuario presiona submit
+    const onSubmitProtecto=(e) => {
+        e.preventDefault();
+
+        //Validar el proyecto
+
+        //agregar al state
+
+        //reiniciar el form
+    }
     return (
         <> 
             <button
@@ -10,12 +37,15 @@ const NuevoProyecto = () => {
 
             <form
                 className="formulario-nuevo-proyecto"
+                onSubmit={onSubmitProtecto}
             >
                 <input 
                     type="text"
                     className="input-text"
                     placeholder="Nombre Proyecto"
                     name="nombre"
+                    value={nombre}
+                    onChange={onChangeProyecto}
 
                 />
                 
